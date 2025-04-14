0ef2bff1535930f38219638b9584da8533c83294
Manjunath Tirakannavar <manjunathbt2067@gmail.com>
2025-04-14 21:14:24 +0530

    Initial Commit

---
+++ src/main/java/com/example/spring_gpt_ai/SpringGptAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_gpt_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGptAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGptAiApplication.class, args);
+	}
+
+}
